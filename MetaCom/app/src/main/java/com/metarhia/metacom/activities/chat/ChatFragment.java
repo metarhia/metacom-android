d0639ea404df85e13eb87ba6caa8fe742465868a
Maria <maria.kokshaikina@gmail.com>
2017-06-05 16:12:47 +0300

    display onclick on incoming file message

--- MetaCom/app/src/main/java/com/metarhia/metacom/activities/chat/ChatFragment.java
+++ MetaCom/app/src/main/java/com/metarhia/metacom/activities/chat/ChatFragment.java
@@ -1,19 +1,21 @@
 package com.metarhia.metacom.activities.chat;
 
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.metarhia.metacom.R;
 import com.metarhia.metacom.interfaces.MessageListener;
@@ -37,25 +39,19 @@ public class ChatFragment extends Fragment implements MessageListener, MessageSe
 
     private static final String KEY_CONNECTION_ID = "keyConnectionId";
     private static final String KEY_CHAT_ROOM_NAME = "keyChatRoomName";
-
+    private static final int PICK_IMAGE = 0;
     @BindView(R.id.toolbar_title)
     TextView mToolbarTitle;
-
     @BindView(R.id.toolbar_back)
     ImageView mToolbarBack;
-
     @BindView(R.id.attach)
     ImageView mFileAttach;
-
     @BindView(R.id.send)
     ImageView mSendMessage;
-
     @BindView(R.id.messages_list)
     ListView mMessagesListView;
-
     @BindView(R.id.input_message)
     TextInputEditText mInputMessage;
-
     private Unbinder mUnbinder;
     private ArrayList<Message> mMessages;
     private MessageAdapter mMessageAdapter;
@@ -89,28 +85,38 @@ public class ChatFragment extends Fragment implements MessageListener, MessageSe
             mMessages = new ArrayList<>();
             mMessageAdapter = new MessageAdapter(getActivity(), mMessages);
             mMessagesListView.setAdapter(mMessageAdapter);
+            mMessagesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    Message message = mMessages.get(i);
+                    if (message.isIncoming() && message.getType() == MessageType.FILE) {
+                        // todo download file
+                        ((TextView) ButterKnife.findById(view, R.id.message_left_text)).setText(getString(R.string.downloading));
+                    }
+                }
+            });
 
-            getMessageQueueExamples();
+//            getMessageQueueExamples();
 
         }
         return v;
     }
 
-    private void getMessageQueueExamples() {
-        for (int i = 0; i < 10; i++) {
-            String mes = "";
-            for (int j = 0; j < 10; j++) {
-                mes = mes.concat(i + "");
-            }
-            final Message message = new Message(MessageType.TEXT, mes, (i % 2 == 0));
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    onMessageReceived(message);
-                }
-            }, 500 * i);
-        }
-    }
+//    private void getMessageQueueExamples() {
+//        for (int i = 0; i < 10; i++) {
+//            String mes = "";
+//            for (int j = 0; j < 10; j++) {
+//                mes = mes.concat(i + "");
+//            }
+//            final Message message = new Message(MessageType.TEXT, mes, (i % 2 == 0));
+//            new Handler().postDelayed(new Runnable() {
+//                @Override
+//                public void run() {
+//                    onMessageReceived(message);
+//                }
+//            }, 500 * i);
+//        }
+//    }
 
     @Override
     public void onMessageReceived(Message message) {
@@ -136,8 +142,7 @@ public class ChatFragment extends Fragment implements MessageListener, MessageSe
 
     @OnClick(R.id.attach)
     public void onFileAttachClick() {
-        // todo choose file to send
-        Toast.makeText(getContext(), "file was sent", Toast.LENGTH_SHORT).show();
+        showFileChooser();
     }
 
     @OnClick(R.id.send)
@@ -155,6 +160,32 @@ public class ChatFragment extends Fragment implements MessageListener, MessageSe
     public void onDestroyView() {
         super.onDestroyView();
         mUnbinder.unbind();
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("*/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select file"), PICK_IMAGE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE && resultCode == Activity.RESULT_OK) {
+            Uri fileUri = data.getData();
+
+            // TODO use stream from fileUri to upload file
+            String fileName = fileUri.toString();
+            fileName = fileName.substring(fileName.lastIndexOf('/') + 1, fileName.length());
+            final Message message = new Message(MessageType.FILE, String.format(getString(R.string.uploaded_file), fileName), false);
+            onMessageReceived(message);
+
+            // todo remove
+            final Message message1 = new Message(MessageType.FILE, String.format(getString(R.string.uploaded_file), fileName), true);
+            onMessageReceived(message1);
+        }
     }
 
     public class MessageAdapter extends BaseAdapter {
@@ -192,11 +223,11 @@ public class ChatFragment extends Fragment implements MessageListener, MessageSe
                 view = lInflater.inflate(R.layout.message, parent, false);
             }
 
-            Message m = messages.get(position);
+            final Message m = messages.get(position);
 
             View messageRightLayout = ButterKnife.findById(view, R.id.message_right_layout);
             View messageLeftLayout = ButterKnife.findById(view, R.id.message_left_layout);
-            TextView messageLeftText = ButterKnife.findById(view, R.id.message_left_text);
+            final TextView messageLeftText = ButterKnife.findById(view, R.id.message_left_text);
             TextView messageRightText = ButterKnife.findById(view, R.id.message_right_text);
             if (m.isIncoming()) {
                 messageRightLayout.setVisibility(View.GONE);
